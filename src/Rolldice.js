137b9fa08c78b1b4f47653116df85dde63ce02ae
Mocktar <mocktarissa@gmail.com>
2019-10-05 14:21:35 +0300

    Added a randomizer

--- src/Rolldice.js
+++ src/Rolldice.js
@@ -5,14 +5,29 @@ class Rolldice extends React.Component{
     constructor(props){
         super(props);
         this.state={
-
+    
+            current1:'one',
+            current2:'one'
+            
         }
+        this.randr=this.randr.bind(this)
+    }
+    randr(){
+        let temp=['one','two',
+        'three',
+        'four',
+        'five',
+        'six'];
+        
+        this.setState({current1:temp[Math.floor(Math.random()*6)]})
+        this.setState({current2:temp[Math.floor(Math.random()*6)]})
     }
     render(){
         return(
             <div>
-                <Dice face="two"></Dice>
-                <Dice face="six"></Dice>
+                <Dice face={this.state.current1}></Dice>
+                <Dice face={this.state.current2}></Dice>
+                <button onClick={this.randr}>Randomize</button>
             </div>
         )
     }
